f8aa99d1082b7497184fcb99e3dc78fb4b5da551
Andrés Ladino <122582963+ladinofel@users.noreply.github.com>
2023-09-30 16:51:28 -0500

    Add playRound module which listens for clicks and modifies the DOM

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 const gameBoard = (() => {
   let gameboard = [
-    1, 2, 3,
-    4, 5, 6,
-    7, 8, 9 
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0,
   ];
-
-  const populateBoard = () => {
-    let cells = document.querySelectorAll('.cell');
+  let cells = document.querySelectorAll('.cell');
+  
+  const populateBoard = () => {    
     for(let i = 0; i < cells.length; i++){
       console.log(cells[i]);
       cells[i].textContent = gameboard[i];
     }
-  
-    //gameboard.forEach((item) => console.log(item));
-    //console.log(cells);
   }
 
-  return {populateBoard};
+  return {cells, gameboard, populateBoard};
 
 })();
 
-gameBoard.populateBoard();
+//gameBoard.populateBoard();
 
+
+const playRound = ( () => {
+  const {gameboard} = gameBoard;
+  const {cells} = gameBoard;
+  const {populateBoard} = gameBoard;
+
+  const modifyGameboard = () => {
+    cells.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        gameboard[btn.id] = btn.id;
+        console.log(btn.id);
+        populateBoard();
+      })
+    })
+    //gameboard[0] = "test";
+    //console.log(gameboard);
+  }
+
+  return {modifyGameboard};
+})();
+
+playRound.modifyGameboard();
